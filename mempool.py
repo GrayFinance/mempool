c2721ead78d991e1bba2a402f6471d274f6df75d
leffw <93302536+leffw@users.noreply.github.com>
2023-12-19 17:11:25 -0300

    feat: add `get_address`

--- mempool.py
+++ mempool.py
@@ -4,7 +4,8 @@ class Mempool:
 
     def __init__(self) -> None:
         self.__url = "https://mempool.space"
-
+        self.__ln_url = "https://liquid.network" 
+    
     def get_node_stats(self, id: str) -> dict:
         url = f"{self.__url}/api/v1/lightning/nodes/{id}"
         return requests.get(url).json()
@@ -13,3 +14,10 @@ class Mempool:
         url = f"{self.__url}/api/v1/fees/recommended"
         return requests.get(url).json()
 
+    def get_address(self, address: str, network="bitcoin"):
+        if network == "liquid":
+            url = f"{self.__ln_url}/api/address/{address}"
+        else:
+            url = f"{self.url}/api/address/{address}"
+        
+        return requests.get(url).json()
